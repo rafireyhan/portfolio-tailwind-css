20b4684c56c97abb689d830062df6788fb92d558
rafireyhan <redan215@gmail.com>
2022-12-20 20:35:31 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "8px",
+    },
+    extend: {
+      colors: {
+        primary: "#713f12",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
